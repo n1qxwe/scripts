6f92367d8ef40deff18e289f0930273c17381594
n1qxwe <130807948+n1qxwe@users.noreply.github.com>
2023-06-06 15:25:14 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,2 +1,2 @@
-loadstring(game:httpget("https://raw.githubusercontent.com/n1qxwe/scripts/main/1st.lua"))()
-loadstring(game:httpget("https://raw.githubusercontent.com/n1qxwe/scripts/main/2nd.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/n1qxwe/scripts/main/1st.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/n1qxwe/scripts/main/2nd.lua"))()
