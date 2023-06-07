3409e3fbed80994e37e0ad94c6d65deef2fd97de
n1qxwe <130807948+n1qxwe@users.noreply.github.com>
2023-06-07 17:53:11 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,10 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/n1qxwe/scripts/main/1st.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/n1qxwe/scripts/main/2nd.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/n1qxwe/scripts/main/4th.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/n1qxwe/scripts/main/3rd.lua"))()
+--[[
+you can see modules at documentation
+by nikz#0255
+517916977545609227
+--]]
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/n1qxwe/scripts/main/1st.lua"))() --loads first module
+loadstring(game:HttpGet("https://raw.githubusercontent.com/n1qxwe/scripts/main/2nd.lua"))() --loads second module
+loadstring(game:HttpGet("https://raw.githubusercontent.com/n1qxwe/scripts/main/4th.lua"))() --loads fourth module
+loadstring(game:HttpGet("https://raw.githubusercontent.com/n1qxwe/scripts/main/3rd.lua"))() --loads thitd module
