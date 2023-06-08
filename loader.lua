b8d9315c509eed83bad58afc4e79dbc287325c49
n1qxwe <130807948+n1qxwe@users.noreply.github.com>
2023-06-08 22:11:52 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -4,9 +4,9 @@ by nikz#0255
 517916977545609227
 --]]
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/n1qxwe/scripts/main/preload.lua"))() -- loads preload assets
+loadstring(game:HttpGet("https://raw.githubusercontent.com/n1qxwe/scripts/main/preload.lua"))() --preloads assets
 loadstring(game:HttpGet("https://raw.githubusercontent.com/n1qxwe/scripts/main/5th.lua"))() --loads fifth module
-loadstring(game:HttpGet("https://raw.githubusercontent.com/n1qxwe/scripts/main/1st.lua"))() --loads first module
+loadstring(game:HttpGet("https://raw.githubusercontent.com/n1qxwe/scripts/main/1st.lua"))() --loads first modul
 loadstring(game:HttpGet("https://raw.githubusercontent.com/n1qxwe/scripts/main/2nd.lua"))() --loads second module
 loadstring(game:HttpGet("https://raw.githubusercontent.com/n1qxwe/scripts/main/4th.lua"))() --loads fourth module
 loadstring(game:HttpGet("https://raw.githubusercontent.com/n1qxwe/scripts/main/3rd.lua"))() --loads third module
