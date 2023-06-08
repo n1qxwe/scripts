2d25bc21240a42a8eb48e5cefb3d1668835fce50
n1qxwe <130807948+n1qxwe@users.noreply.github.com>
2023-06-08 20:18:25 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -3,7 +3,7 @@ you can see all modules at documentation (https://raw.githubusercontent.com/n1qx
 by nikz#0255
 517916977545609227
 --]]
-
+loadstring(game:HttpGet("https://raw.githubusercontent.com/n1qxwe/scripts/main/5th.lua"))() --loads fifth module
 loadstring(game:HttpGet("https://raw.githubusercontent.com/n1qxwe/scripts/main/1st.lua"))() --loads first module
 loadstring(game:HttpGet("https://raw.githubusercontent.com/n1qxwe/scripts/main/2nd.lua"))() --loads second module
 loadstring(game:HttpGet("https://raw.githubusercontent.com/n1qxwe/scripts/main/4th.lua"))() --loads fourth module
