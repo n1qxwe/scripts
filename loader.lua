9c0c591c12a30b2ebd8fbaee4eed58bb122ce744
n1qxwe <130807948+n1qxwe@users.noreply.github.com>
2023-06-09 18:05:27 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,9 +2,15 @@
 you can see all modules at documentation (https://raw.githubusercontent.com/n1qxwe/scripts/main/README.md)
 by nikz#0255
 517916977545609227
+NOTE: Assets preloading may crash at old and/or slow pc.
 --]]
 
---assets preloading may crash at slow pc
+    if _G.Preload == true then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/n1qxwe/scripts/main/preload.lua"))()
+    else
+        print("preload canceled")
+    end
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/n1qxwe/scripts/main/5th.lua"))() --loads fifth module
 loadstring(game:HttpGet("https://raw.githubusercontent.com/n1qxwe/scripts/main/1st.lua"))() --loads first modul
 loadstring(game:HttpGet("https://raw.githubusercontent.com/n1qxwe/scripts/main/2nd.lua"))() --loads second module
